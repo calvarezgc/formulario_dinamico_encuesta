52e7ab710d25ec65fb02e201c06d088ea8836f4a
Cristina Alvarez <cristina.alvarezgc@gmail.com>
2022-10-18 09:02:01 +0200

    update post.php

--- post.php
+++ post.php
@@ -6,10 +6,11 @@ $question3 = $_POST["question3"]
 $nombre = $_POST["name"]
 $email = $_POST["Email"]
 $tel= $_POST["Tel"]
+$terms = $_POST["condiciones"]
 
-$data = [$question1, $question2, $question3, $nombre, $email, $tel];
+$data = [$question1, $question2, $question3, $nombre, $email, $tel, $terms];
 
-if($question1 == "" || $question2 == "" ||$question3 == "" || $nombre == ""|| $email == "" || $tel == ""){
+if($question1 == "" || $question2 == "" ||$question3 == "" || $nombre == ""|| $email == "" || $tel == "" || $terms == ""){
   echo json_encode(false);
 }else{
   echo json_encode($data);
