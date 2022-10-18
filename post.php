51be4ac1edd2ecbb9b5538355aae37a485690b8b
Cristina Alvarez <cristina.alvarezgc@gmail.com>
2022-10-18 09:38:29 +0200

    m

--- post.php
+++ post.php
@@ -1,5 +1,14 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+
 $question1 = $_POST["question1"]
 $question2 = $_POST["question2"]
 $question3 = $_POST["question3"]
